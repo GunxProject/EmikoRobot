d1efb338b5fde6d335e193ef38186b6cbb4373e0
Gunsila <89563674+GunxProject@users.noreply.github.com>
2022-09-28 14:37:57 +0700

    Update asupan.py

--- EmikoRobot/modules/asupan.py
+++ EmikoRobot/modules/asupan.py
@@ -89,7 +89,7 @@ async def _(event):
         ]
         kontols = random.choice(ayangnya)
         pantek = await ubot2.download_media(kontols)
-        aku = await ubot2.get_users()
+        aku = await tbot.get_user()
         await tbot.send_file(
             event.chat_id,
             caption=f"**Nih Hasianmu [{aku.first_name}](tg://user?id={aku.id}). Jaga Baik-Baik. Awas Lepas**.",
